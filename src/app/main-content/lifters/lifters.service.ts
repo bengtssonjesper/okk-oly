e2b8476284d1bbcc78c522d8cb6d3b10d9c8d202
Jesper Bengtsson <jesperbengtsson2@gmail.com>
2021-09-22 09:39:50 +0200

    Changed weight so all have different PR's

--- src/app/main-content/lifters/lifters.service.ts
+++ src/app/main-content/lifters/lifters.service.ts
@@ -9,15 +9,15 @@ export class LiftersService {
 
 
     ourLifters: Lifter[] =[
-      new Lifter("1", "Jesper B", 73, 107, 125, 220, {imageURL:"https://i.insider.com/60f7f5a70729770012b90039?width=700"}),
-      new Lifter("2", "Albin", 73, 107, 125, 220, {imageURL:"https://www.mercurynews.com/wp-content/uploads/2021/08/BNG-L-OLYLIFT-3.jpg"}),
-      new Lifter("3", "Linus", 73, 107, 125, 220,{bio:"some bio",imageURL:"https://i0.wp.com/www.mercurynews.com/wp-content/uploads/2021/08/BNG-L-OLYLIFT-1.jpg?fit=620,9999px&ssl=1"}),
-      new Lifter("4", "Olle", 73, 107, 125, 220, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
-      new Lifter("5", "Fredrik", 73, 107, 125, 220, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
-      new Lifter("6", "Jesper H", 73, 107, 125, 220, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
-      new Lifter("7", "Amanda", 73, 107, 125, 220, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
-      new Lifter("8", "Lina", 73, 107, 125, 220, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
-      new Lifter("9", "Wilma", 73, 107, 125, 220, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
+      new Lifter("1", "Jesper B", 73, 107, 125, 228, {imageURL:"https://i.insider.com/60f7f5a70729770012b90039?width=700"}),
+      new Lifter("2", "Albin", 89, 106, 124, 227, {imageURL:"https://www.mercurynews.com/wp-content/uploads/2021/08/BNG-L-OLYLIFT-3.jpg"}),
+      new Lifter("3", "Linus", 73, 105, 123, 226,{bio:"some bio",imageURL:"https://i0.wp.com/www.mercurynews.com/wp-content/uploads/2021/08/BNG-L-OLYLIFT-1.jpg?fit=620,9999px&ssl=1"}),
+      new Lifter("4", "Olle", 96, 104, 122, 225, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
+      new Lifter("5", "Fredrik", 81, 103, 121, 224, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
+      new Lifter("6", "Jesper H", 81, 102, 120, 223, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
+      new Lifter("7", "Amanda", 64, 101, 119, 222, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
+      new Lifter("8", "Lina", 59, 100, 118, 221, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
+      new Lifter("9", "Wilma", 71, 99, 117, 220, {imageURL:"https://d.newsweek.com/en/full/1855213/team-usas-jourdan-elizabeth-delacruz.jpg?w=790&f=a7ff3c88bd2c02056a1694d011e03a54"}),
       
       ]
 
